d4019791da4b0dc2fbbb8ce6bc1d9317e9c8b1c0
l3eniy <pt!gD%f6%^01>
2022-05-15 23:55:19 +0200

    fx

--- src/js/index1.js
+++ src/js/index1.js
@@ -1,4 +1,4 @@
-import bring from 'bring.js'
+import bring from '../js/bring.js'
 
 var BRING_ICON = 'https://global-uploads.webflow.com/5fbe6548a005d56f0dd39a2e/5fc24a65f7e1555200865e1b_bring-logo.svg';
 var Promise = TrelloPowerUp.Promise

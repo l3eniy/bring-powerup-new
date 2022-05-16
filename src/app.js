e5938091c3bab06ec441f3d3e7e1b0cdaf0735e6
l3eniy <pt!gD%f6%^01>
2022-05-16 21:48:53 +0200

    dddv

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 var express = require("express");
-const bringApi = require(`bring-shopping`);
+// const bringApi = require(`bring-shopping`);
 const serverless = require('serverless-http')
 
 
 const app = express();
 const router = express.Router();
 
-app.use(express.static('src'));
+//app.use(express.static('src'));
 
-const bring = new bringApi({mail: `benjamin.fuhlbruegge@gmail.com`, password: `9PiC!TSxnRXLrG&Q`});
+//const bring = new bringApi({mail: `benjamin.fuhlbruegge@gmail.com`, password: `9PiC!TSxnRXLrG&Q`});
 
 
 
-app.get("/", async (req, res, next) => {
-    try {
-        await bring.login();
-        console.log(`Successfully logged in as ${bring.name}`);
-    } catch (e) {
-        console.error(`Error on Login: ${e.message}`);
-    }
+// app.get("/", async (req, res, next) => {
+//     try {
+//         await bring.login();
+//         console.log(`Successfully logged in as ${bring.name}`);
+//     } catch (e) {
+//         console.error(`Error on Login: ${e.message}`);
+//     }
 
-    var x = await bring.loadLists();
-    res.json(x);
-   });
+//     var x = await bring.loadLists();
+//     res.json(x);
+//    });
 
    app.get("/test", (req, res, next) => {
     res.json({"hello" : "test"});
